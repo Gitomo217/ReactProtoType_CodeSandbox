d0fb2438968782b52121052fc2f2608655865a01
Gitomo217 <doublemoon12@yahoo.co.jp>
2022-05-06 09:35:28 +0900

    html/cssひな形作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,37 @@
+import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="input-area">
+        <input placeholder="入力エリア" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="tytle">未完了のタスク</p>
+        <ul>
+          <div className="list-row">
+            <li>タスク１</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+          <div className="list-row">
+            <li>タスク２</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="tytle">完了済タスク</p>
+        <ul>
+          <div className="list-row">
+            <li>タスク２</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
